b80a95c92a605c2e27a550a43cd853977f262012
halitanildonmez <halitanil.donmez@gmail.com>
2018-01-27 23:17:23 +0100

    added dijsktra, not working atm

--- skynet-revolution-ep-2/submisson.cpp
+++ skynet-revolution-ep-2/submisson.cpp
@@ -6,16 +6,65 @@
 #include <queue>
 using namespace std;
 
-int findMin (int *arr, int N) {
-    int maxVal = 100;
-    int index = -1;
+int distToStart (int start, int end, vector<int> adjGraph[], int N, vector<int> nodes, int &p) {
+    int *distances = new int[N];
+    bool *visited = new bool [N];
+    vector <int> prev[N];
     for (int i = 0; i < N; i++) {
-        if (arr [i] < maxVal) {
-            maxVal = arr [i];
-            index = i;
+        distances[i] = 500;
+        visited[i] = false;
+    }
+    
+    distances [start] = 0;
+
+    while (!nodes.empty()) {
+        int minDist = 500;
+        int curNode = -1;
+        int delIndex = -1;
+        for (int i = 0; i < nodes.size (); i++) {
+            int n = nodes[i];
+            if (distances[n] < minDist && !visited[n]) {
+                curNode = n;
+                minDist = distances[n];
+                delIndex = i;
+            }
+        }
+        if (delIndex == -1)
+            break;
+        if (curNode == end) {
+            break;
+        }
+        nodes.erase (nodes.begin () + delIndex);
+        visited[curNode] = true;
+        vector<int> neighs = adjGraph[curNode];
+        for (int i = 0; i < neighs.size(); i++) {
+            int newDist = distances[curNode] + 1;
+            if (newDist < distances[neighs[i]]) {
+                distances[neighs[i]] = newDist;
+                prev[neighs[i]].push_back(curNode);
+            }
         }
     }
-    return index;
+    cerr << "start" << start << " end " << end << endl;
+    int retVal = distances[end];
+    p = prev[end][0];
+    cerr << "SADASD " << p << endl;
+    for (int i = 0; i < N; i++) {
+        vector<int> tmpVexc = prev[i];
+        cerr << "for " << i << " ";
+        for (int j = 0; j < tmpVexc.size (); j++) {
+            cerr << tmpVexc[j];
+            if (tmpVexc[j] == end) {
+                p = tmpVexc[j];
+                cerr << p << " PP " << endl;
+                break;
+            }
+        }
+        cerr << endl;
+    }
+    delete[]distances;
+    delete[]visited;
+    return retVal;
 }
 
 /**
@@ -29,9 +78,11 @@ int main()
     int E; // the number of exit gateways
     cin >> N >> L >> E; cin.ignore();
     
-    //Graph graph (N);
+    bool *visited = new bool [N];
+    
     vector<int> adjGraph[N]; 
     vector<int> gatewayNode;
+    vector<int> nodes (N);
     
     for (int i = 0; i < L; i++) {
         int N1; // N1 and N2 defines a link between these nodes
@@ -39,141 +90,37 @@ int main()
         cin >> N1 >> N2; cin.ignore();
         adjGraph[N1].push_back(N2);
         adjGraph[N2].push_back(N1);
+        visited[N1] = false;
     }
     for (int i = 0; i < E; i++) {
         int EI; // the index of a gateway node
         cin >> EI; cin.ignore();
         gatewayNode.push_back(EI);
-    }
-    
-    vector<int> q;
-    bool *visited = new bool[N];
-    int *distances = new int [N];
-    for(int i = 0; i < N; i++) {
-        visited[i] = false;
-        distances [i] = 100;
-        q.push_back (i);
-    }
         
-    
-    int startNode = gatewayNode[0];
-    
-    
-    vector<int> prev[N];
-    distances[startNode] = 0;
-    
-    visited[startNode] = true;
-    
-    while (!q.empty()) {
-        int u = findMin (distances, N);
-        vector <int>::iterator minElem = q.begin();
-        minElem = std::find (q.begin (), q.end(), u);
-        if (minElem != q.end ()) {
-            q.erase (q.begin () + std::distance (q.begin (), minElem));
-        }
-        for (std::vector<int>::iterator it = adjGraph[u].begin() ; it != adjGraph[u].end(); ++it) {
-            int dist = distances [u] + 1;
-            if (dist < distances [*it]) {
-                distances [*it] = dist;
-                prev [*it].push_back(u);
-            }
-        }
     }
     
+    for (int i = 0; i < N; i++) {
+        nodes.push_back(i);
+    }
     // game loop
     while (1) {
         int SI; // The index of the node on which the Skynet agent is positioned this turn
         cin >> SI; cin.ignore();
-        
-        bool alreadySevered = false;
-        vector <int> placesAgentCanGo = adjGraph [SI];
-        int index = 0;
-        for (std::vector<int>::iterator it = placesAgentCanGo.begin() ; it != placesAgentCanGo.end(); ++it) {
-            int y = *it;
-            if (std::find(gatewayNode.begin(), gatewayNode.end(), y) != gatewayNode.end()) {
-                index = y;
-                alreadySevered = true;
-                break;
+        int minVal = 500;
+        int gateX = -1;
+        int gateY = -1;
+        int hh = -1;
+        for (int i = 0; i < E; i++) {
+            int curVal = distToStart(SI, gatewayNode[i], adjGraph, N, nodes, gateY);
+            int gIndex = gatewayNode[i];
+            if (curVal < minVal && (!visited[gateY])) {
+                minVal = curVal;
+                gateX = gatewayNode[i];
+                hh = gateY;
             }
         }
-        if (alreadySevered) {
-            cout << SI << " " << index << endl;
-        }
-        else{
-            std::queue<int> queue;
-            cerr << "entered here for " << SI << endl;
-
-            // Write an action using cout. DON'T FORGET THE "<< endl"
-            // To debug: cerr << "Debug messages..." << endl;
-            // Example: 3 4 are the indices of the nodes you wish to sever the link between
-            cout << gatewayNode.at (0) << " " << adjGraph[gatewayNode.at (0)][0] << endl;   
-        }
- 
+        visited [gateY] = true;
+        cout << gateX << " " << hh << endl;
+   
     }
 }
-/*
-            queue.push(SI);
-            visited [SI] = true;
-            while (!queue.empty()) {
-                int cur = queue.front();
-                queue.pop ();
-                if (std::find(gatewayNode.begin(), gatewayNode.end(), cur) != gatewayNode.end()) {
-                    vector<int> allAdj = adjGraph[cur];
-                    int tmp = allAdj.at (0);
-                    for (std::vector<int>::iterator it2 = allAdj.begin() ; it2 != allAdj.end(); ++it2) {
-                        int curNeigh = *it2;
-                        if ( curNeigh == SI) {
-                            tmp = curNeigh;
-                        }
-                    }
-                    if (!alreadySevered) {
-                        cerr << "HUH" << endl;
-                        cout << cur << " " << tmp << endl;
-                    }
-                        
-                }
-                
-                vector <int> adjacentNodes = adjGraph[cur];
-                for (std::vector<int>::iterator it3 = adjacentNodes.begin() ; it3 != adjacentNodes.end(); ++it3) {
-                    int neigh = *it3;
-                    if (!visited[neigh]) {
-                        queue.push (neigh);
-                        visited [neigh] = true;
-                    }
-                }
-                
-            }
-*/
-/*
-            queue.push(SI);
-            visited [SI] = true;
-            while (!queue.empty()) {
-                int cur = queue.front();
-                queue.pop ();
-                if (std::find(gatewayNode.begin(), gatewayNode.end(), cur) != gatewayNode.end()) {
-                    vector<int> allAdj = adjGraph[cur];
-                    int tmp = allAdj.at (0);
-                    for (std::vector<int>::iterator it2 = allAdj.begin() ; it2 != allAdj.end(); ++it2) {
-                        int curNeigh = *it2;
-                        if ( curNeigh == SI) {
-                            tmp = curNeigh;
-                        }
-                    }
-                    if (!alreadySevered) {
-                        cerr << "HUH" << endl;
-                        cout << cur << " " << tmp << endl;
-                    }
-                        
-                }
-                
-                vector <int> adjacentNodes = adjGraph[cur];
-                for (std::vector<int>::iterator it3 = adjacentNodes.begin() ; it3 != adjacentNodes.end(); ++it3) {
-                    int neigh = *it3;
-                    if (!visited[neigh]) {
-                        queue.push (neigh);
-                        visited [neigh] = true;
-                    }
-                }
-                
-            }
-*/

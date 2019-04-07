4c7bcee5d9ad4791bb5c55fe55508ae5dd8c4933
halitanildonmez <halitanil.donmez@gmail.com>
2019-04-07 16:59:09 +0200

    trilateration seems to be working for a simple solution

--- shadows-of-the-knight-e2-very-hard/solution.cpp
+++ shadows-of-the-knight-e2-very-hard/solution.cpp
@@ -2,8 +2,24 @@
 #include <string>
 #include <vector>
 #include <algorithm>
+#include <math.h>
 
 using namespace std;
+
+struct Point2D
+{
+    float x;
+    float y;
+    Point2D (int cx, int cy):x(cx), y(cy) {}
+};
+
+Point2D trilateral (Point2D p1, Point2D p2, Point2D p3, float r1, float r2,
+                    float r3)
+{
+    float x = (r1*r1 - r2*r2 + p2.x*p2.x) / (2*p2.x);
+    float y = (r1*r1 - r3*r3 + p3.x*p3.x + p3.y*p3.y - 2*p3.x*x) / (2*p3.y);
+    return Point2D(x,y);
+}
 
 /**
  * Auto-generated code below aims at helping you parse
@@ -20,14 +36,28 @@ int main()
     int Y0;
     cin >> X0 >> Y0; cin.ignore();
 
+    Point2D p1(0,0);
+    Point2D p2(W,0);
+    Point2D p3(X0,H);
+    
     // game loop
     while (1) {
         string bombDir; // Current distance to the bomb compared to previous distance (COLDER, WARMER, SAME or UNKNOWN)
         cin >> bombDir; cin.ignore();
+        
+        if (bombDir == "COLDER"){
+            cerr << "COLDER" << endl;       
+        }
+        else if (bombDir == "WARMER"){
+            cerr << "WARMER" << endl;
+            p2 = Point2D(X0, 0);
+            
+        }
+        else if (bombDir == "SAME") {
+            cerr << "SAME" << endl;
+        }
 
-        // Write an action using cout. DON'T FORGET THE "<< endl"
-        // To debug: cerr << "Debug messages..." << endl;
-
-        cout << "0 0" << endl;
+        Point2D tri = trilateral(p1, p2, p3, 3.f, 1.f, 1.f);
+        cout << tri.x << " " << tri.y << endl;
     }
 }

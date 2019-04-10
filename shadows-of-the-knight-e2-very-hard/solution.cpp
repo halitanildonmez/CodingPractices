daa74ff422a58968231456abf3b91f61a03a2c49
halitanildonmez <halitanil.donmez@gmail.com>
2019-04-10 22:12:25 +0200

    passing second failing first, going to revisit but found logic for the problem, also need to handle cold better

--- shadows-of-the-knight-e2-very-hard/solution.cpp
+++ shadows-of-the-knight-e2-very-hard/solution.cpp
@@ -13,6 +13,10 @@ struct Point2D
     Point2D (int cx, int cy):x(cx), y(cy) {}
 };
 
+struct Box {
+    float x0, x1, x2, x3;
+};
+
 Point2D trilateral (Point2D p1, Point2D p2, Point2D p3, float r1, float r2,
                     float r3)
 {
@@ -30,6 +34,14 @@ Point2D boxApp (int X0, int Y0, int W, int H, int sizeX, int sizeY)
     if (b2 > H)
         b2 = H-1;
     return Point2D(b1,b2);
+}
+
+bool withinBound (int val, int bound) {
+    return val < bound;
+}
+
+void debugPoint(Point2D p) {
+    cerr << p.x << " " << p.y << endl;
 }
 
 /**
@@ -51,40 +63,65 @@ int main()
     Point2D p2(W,0);
     Point2D p3(X0,H);
     
+    Point2D area(W, H);
+    
     int bx = 3;
     int by = 3;
     
+    int midX = W / 2;
+    int midY = H / 2;
+    
     cerr << W << " " << H << endl;
+    
+    bool isCold = false;
     
     // game loop
     while (1) {
         string bombDir; // Current distance to the bomb compared to previous distance (COLDER, WARMER, SAME or UNKNOWN)
         cin >> bombDir; cin.ignore();
         
-        if (bombDir == "COLDER"){
-            cerr << "COLDER" << endl;       
-        }
-        else if (bombDir == "WARMER"){
+        
+        
+        if (bombDir == "COLDER") {
+            cerr << "COLDER" << endl;
+            isCold = true;
+            // 0, 1
+            debugPoint(Point2D(midX,midY));
+            
+        } else if (bombDir == "WARMER") {
             cerr << "WARMER" << endl;
-            //p2 = Point2D(X0, 0);
-            if (bx > 0)
-                bx--;
-            if (by > 0)
-                by--;
-        }
-        else if (bombDir == "SAME") {
+            area = Point2D(W, midY);
+            debugPoint(area);
+                        
+            X0 = midX;
+            Y0 = midY;
+        } else if (bombDir == "SAME") {
             cerr << "SAME" << endl;
-
         }
 
+        if (!isCold) {
+            midX /= 2;
+            midY /= 2;   
+            if (!withinBound(midX, W))
+                midX = W - 1;
+            if (!withinBound(midY, H))
+                midY = H - 1;
+            
+            cout << midX << " " << midY << endl;
+        } else {
+            midX++;
+            cout << midX << " " << midY << endl;
+        }
+    }
+}
+
+        /*
         Point2D tri = boxApp(X0, Y0, W, H, bx, by);
         X0 = tri.x;
         Y0 = tri.y;
         cout << tri.x << " " << tri.y << endl;
-        
+        */
         /*
         Point2D tri = trilateral(p1, p2, p3, 3.f, 1.f, 1.f);
         cout << tri.x << " " << tri.y << endl;
         */
-    }
-}

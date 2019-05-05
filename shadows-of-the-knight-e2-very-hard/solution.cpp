16b591f9457431db72d5f31030e21c8cf29149d8
halitanildonmez <halitanil.donmez@gmail.com>
2019-05-05 16:34:37 +0200

    solve 1 and 2 sub no work with 1, need to update log when cold

--- shadows-of-the-knight-e2-very-hard/solution.cpp
+++ shadows-of-the-knight-e2-very-hard/solution.cpp
@@ -22,13 +22,27 @@ int to1DCoord (int x, int y, int w) {
 }
 
 Point2D to2DCoord (int i, int w) {
-    int x = i / w;
-    int y = i % w;
+    int y = i / w;
+    int x = i % w;
     return Point2D(x, y);
+}
+
+float EuclidianDistance (Point2D p1, Point2D p2) {
+    float A = abs(p2.x - p1.x);
+    float B = abs(p2.y - p1.y);
+    float AB = (A*A) + (B*B);
+    return sqrtf(AB);
+}
+
+float Euclidian1D (int i1, int i2, int w)  {
+    return EuclidianDistance(to2DCoord(i1, w), to2DCoord(i2, w));
 }
 // ---------- PRINT STATEMENTS ---------------
 void debugPoint(Point2D p) {
     cerr << p.x << " " << p.y << endl;
+}
+void debug1DPoint(int p, int w) {
+    debugPoint(to2DCoord(p, w));
 }
 // -------------------------------------------
 
@@ -55,37 +69,87 @@ int main()
     
     int l = 0;
     int r = (W*H) - 1;
-    int dx = X0;
-    int dy = Y0;
     
+    /**
+        thinking of Jack Sparrow for some reason. we need to know the direction of 
+        which we have to update the left and right. 
+        if true:
+            on warm => update left
+            on cold => update right
+        if false:
+            default behaviour
+    */
+    bool isUpDown = false;
+    
+    int midPoint = floor((l+r)/2);
+    int startIndex = to1DCoord(X0, Y0, W);
+    if (startIndex < midPoint) {
+        cerr << "up is down" << endl;
+        cerr << "dist " << Euclidian1D(startIndex, midPoint, W) << endl;
+        
+        isUpDown = true;
+    }
+    
+    int totalDist = 0;
+    int lastWarmPosition = midPoint;
     int m = 0;
-    Point2D output = Point2D(X0, Y0);
-    // https://stackoverflow.com/questions/1449713/representing-a-2d-array-as-a-1d-array
     // game loop
     while (1) {
         string bombDir; // Current distance to the bomb compared to previous distance (COLDER, WARMER, SAME or UNKNOWN)
         cin >> bombDir; cin.ignore();
         
+        bool wasWarm = false;
+        
         if (bombDir == "COLDER") {
             cerr << "COLDER" << endl;
-            l = m + 1;
-            m = floor((l+r)/2);
-            printRes (m, W);
+            debug1DPoint(lastWarmPosition, W);
+            cerr << "total dist is " << totalDist << endl;
+            if (isUpDown) {
+                // the output distance
+                r = m;
+                // wrong! Should go back last warm - distance somehow
+                l = midPoint;
+                //l = lastWarmPosition; //(wrong)
+            } else {
+                l = m + 1;
+            }
+            
         } else if (bombDir == "WARMER") {
             cerr << "WARMER " << endl;
-            r = m - 1;
-            m = floor((l+r)/2);
-            printRes (m, W);
+            wasWarm = true;
+            if (isUpDown) {
+                l = m + 1;
+            } else {
+                r = m - 1;    
+            }
         } else if (bombDir == "SAME") {
             cerr << "SAME " << X0 << " - " << Y0 << endl;
             l = m + 1;
-            m = floor((l+r)/2);
-            printRes (m, W);
         } else {
             cerr << "Default " << endl;
-            // will enter here only once (on the start)
-            m = floor((l+r)/2);
-            printRes(m, W);
+            // enter here on first try
         }
+        cerr << "Calculating mid between \nLeft:";
+        debug1DPoint(l, W);
+        cerr << "And Right:";
+        debug1DPoint(r, W);
+        // calculate the mid point
+        m = floor((l+r)/2);
+        // print the result
+        printRes (m, W);
+        // calculate the distance to the last posint 
+        // currently for debugging only
+        startIndex = to1DCoord(X0, Y0, W);
+        cerr << "dist final: " << Euclidian1D(m, startIndex, W) << endl;
+        
+        // update the last position where we were warmer
+        if (wasWarm) {
+            totalDist += Euclidian1D(m, startIndex, W);
+            lastWarmPosition = to1DCoord(X0, Y0, W);
+        }
+        // update to the current position
+        Point2D dest = to2DCoord(m, W);
+        X0 = dest.x;
+        Y0 = dest.y;
     }
 }

35434b56e8a0b93293990abc05a6b6dc1ded29e7
halitanildonmez <halitanil.donmez@gmail.com>
2019-10-12 21:11:58 +0200

    adding conversions for meter and km

--- aneo-puzzle-medium/solution.cpp
+++ aneo-puzzle-medium/solution.cpp
@@ -10,9 +10,19 @@ struct Zone
     int dist;
     int dur;
 };
-int calculate_v (int x, int t) 
+float calculate_v (float x, float t) 
 {
     return x/t;
+}
+// meters to kilometers
+float m2km(float distance) 
+{
+    return distance/1000;
+}
+// seconds to hours
+float s2h(float seconds) 
+{
+    return seconds / 3600;
 }
 int main()
 {
@@ -21,31 +31,37 @@ int main()
     int lightCount;
     cin >> lightCount; cin.ignore();
     
-    cerr << "Max speed: " << speed << endl;
-    int time_elapsed = 0;
+    cerr << "Max speed (km/h): " << speed << endl;
+    float time_elapsed = 0;
     int v_out = speed;
     
     for (int i = 0; i < lightCount; i++) {
         int distance;
         int duration;
         cin >> distance >> duration; cin.ignore();
-        cerr << "Distance and duration: " 
+        cerr << "Distance (meter) and duration(seconds): " 
             << distance << " " << duration << endl;
         
+        // Conver to km from meter
+        float km_dist = m2km(distance);
+        float h_dur = s2h(duration);
+        cerr << "KM distance: " << km_dist << " Hour duration: " <<
+            h_dur << "\n";
         
-        int current_time = duration - time_elapsed;
+        float current_time = h_dur - time_elapsed;
         cerr << "elapsed time from the light: " << current_time << endl;
         
-        int v_cur = calculate_v(distance, duration);
+        float v_cur = calculate_v(km_dist, current_time);
+        cerr << "Current calculated speed: " << v_cur << endl;
         // we can use the max speed
         if (v_cur < speed)
             v_cur = speed;
+        else{
+            cerr << "Speed is not enough" << endl;
+        }
             
         v_out = v_cur;
     }
-
-    // Write an action using cout. DON'T FORGET THE "<< endl"
-    // To debug: cerr << "Debug messages..." << endl;
-
-    cout << v_out << endl;
+    cerr << "output speed (km/h): " << v_out << endl;
+    cout << (int)v_out << endl;
 }

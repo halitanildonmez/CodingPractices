295916872cb99967db0358aaa0fd6395c6fb32b0
halitanildonmez <halitanil.donmez@gmail.com>
2019-10-16 21:14:14 +0200

    solved until 4th will change a bit

--- aneo-puzzle-medium/solution.cpp
+++ aneo-puzzle-medium/solution.cpp
@@ -2,6 +2,7 @@
 #include <string>
 #include <vector>
 #include <algorithm>
+#include <math.h>
 
 using namespace std;
 
@@ -19,10 +20,40 @@ float m2km(float distance)
 {
     return distance/1000;
 }
+float msTokmh(float speed) 
+{
+    return speed * 3.6f;
+}
 // seconds to hours
 float s2h(float seconds) 
 {
     return seconds / 3600;
+}
+float check_existing(float speed, float dist, float duration)
+{
+    float t = calculate_v(dist, speed);
+    if (t < duration)
+        return speed;
+    float t_x = t / duration;
+    if (fmod(t_x, 2) == 0)
+        return speed;
+    return -1.0f;
+}
+float calc_speed(float dist, float vmax, float elapsed, float duration) 
+{
+    float v = calculate_v(dist, elapsed);
+    if (v <= vmax) {
+        cerr << "max speed is enough" << endl;
+        return vmax;
+    }
+    int m = 2;
+    while (v > vmax)
+    {
+        v = calculate_v(dist, elapsed * m);
+        cerr << "Speed : " << v << endl;
+        m += 2;
+    }
+    return v;
 }
 int main()
 {
@@ -51,23 +82,21 @@ int main()
         float current_time = h_dur - time_elapsed;
         cerr << "elapsed time from the light: " << current_time << endl;
         
-        float v_cur = calculate_v(km_dist, current_time);
-        cerr << "Current calculated speed: " << v_cur << endl;
-        // we can use the max speed
-        if (v_cur < speed)
-            v_cur = speed;
-        else{
-            cerr << "Speed is not enough" << endl;
-            float new_time = calculate_v(km_dist, speed);
-            if (new_time > h_dur)
-            {
-                float new_speed = calculate_v(km_dist, 2*current_time);
-                cerr << "max speed is too slow " << new_time << endl;
-                if (new_time <= speed)
-                    v_cur = new_speed;
-            }
+        float v_cur = calc_speed(km_dist, speed, current_time, h_dur);
+        
+        if (i > 0 && v_out != speed)
+        {
+            float v_tmp = check_existing(v_out, km_dist, h_dur);
+            if (v_tmp != -1)
+                v_cur = v_tmp;
         }
-            
+        else 
+        {
+            v_cur = calc_speed(km_dist, speed, current_time, h_dur);    
+        }
+        
+       
+        
         v_out = v_cur;
         time_elapsed += calculate_v(km_dist, v_cur);
     }

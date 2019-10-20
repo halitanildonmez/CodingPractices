24bfd6aafc252913d229223ee7b77e1208c84209
halitanildonmez <halitanil.donmez@gmail.com>
2019-10-20 15:15:08 +0200

    solve until 5

--- aneo-puzzle-medium/solution.cpp
+++ aneo-puzzle-medium/solution.cpp
@@ -50,7 +50,7 @@ float calc_speed(float dist, float vmax, float elapsed, float duration)
     while (v > vmax)
     {
         v = calculate_v(dist, elapsed * m);
-        cerr << "Speed : " << v << endl;
+        cerr << "Speed : " << v << " M: " << m << endl;
         m += 2;
     }
     return v;
@@ -62,9 +62,11 @@ int main()
     int lightCount;
     cin >> lightCount; cin.ignore();
     
-    cerr << "Max speed (km/h): " << speed << endl;
+    float maxSpeed = speed / 3.6f;
+    
+    cerr << "Max speed (km/h): " << maxSpeed << endl;
     float time_elapsed = 0;
-    int v_out = speed;
+    float v_out = maxSpeed;
     
     for (int i = 0; i < lightCount; i++) {
         int distance;
@@ -73,33 +75,30 @@ int main()
         cerr << "Distance (meter) and duration(seconds): " 
             << distance << " " << duration << endl;
         
-        // Conver to km from meter
-        float km_dist = m2km(distance);
-        float h_dur = s2h(duration);
-        cerr << "KM distance: " << km_dist << " Hour duration: " <<
-            h_dur << "\n";
-        
-        float current_time = h_dur - time_elapsed;
+        float current_time = duration; //- time_elapsed;
         cerr << "elapsed time from the light: " << current_time << endl;
         
-        float v_cur = calc_speed(km_dist, speed, current_time, h_dur);
-        
-        if (i > 0 && v_out != speed)
+        float v_cur = calc_speed(distance, maxSpeed, current_time, duration);
+        if (v_cur == maxSpeed)
         {
-            float v_tmp = check_existing(v_out, km_dist, h_dur);
+            cerr << " 2 " << endl;
+            v_cur = calc_speed(distance, v_out, current_time, duration);    
+        }
+        else if (i > 0 && v_out != maxSpeed)
+        {
+            cerr << " 1 " << endl;
+            float v_tmp = check_existing(v_out, distance, duration);
             if (v_tmp != -1)
+            {
                 v_cur = v_tmp;
+                speed = v_cur;
+            }
         }
-        else 
-        {
-            v_cur = calc_speed(km_dist, speed, current_time, h_dur);    
-        }
-        
-       
         
         v_out = v_cur;
-        time_elapsed += calculate_v(km_dist, v_cur);
+        cerr << "tmp output " << v_out << endl;
+        time_elapsed += calculate_v(distance, v_cur);
     }
-    cerr << "output speed (km/h): " << v_out << endl;
-    cout << (int)v_out << endl;
+    cerr << "output speed (km/h): " << v_out*3.6f << endl;
+    cout << (int)(v_out*3.6f) << endl;
 }

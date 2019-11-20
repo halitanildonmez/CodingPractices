3d77e5d96093e4e8ab4eec65d9c84e73d3be68c4
halitanildonmez <halitanil.donmez@gmail.com>
2019-11-20 21:54:55 +0100

    final submission

--- aneo-puzzle-medium/solution.cpp
+++ aneo-puzzle-medium/solution.cpp
@@ -3,58 +3,42 @@
 #include <vector>
 #include <algorithm>
 #include <math.h>
+#include <iomanip>
+#include <list>
 
 using namespace std;
 
+// Zone structure, has duration, distance and 
+// all the speed values, Size is some large value
+// though is not needed 
+// 
 struct Zone
 {
-    int dist;
-    int dur;
-};
-float calculate_v (float x, float t) 
-{
-    return x/t;
-}
-// meters to kilometers
-float m2km(float distance) 
-{
-    return distance/1000;
-}
-float msTokmh(float speed) 
-{
-    return speed * 3.6f;
-}
-// seconds to hours
-float s2h(float seconds) 
-{
-    return seconds / 3600;
-}
-float check_existing(float speed, float dist, float duration)
-{
-    float t = calculate_v(dist, speed);
-    if (t < duration)
-        return speed;
-    float t_x = t / duration;
-    if (fmod(t_x, 2) == 0)
-        return speed;
-    return -1.0f;
-}
-float calc_speed(float dist, float vmax, float elapsed, float duration) 
-{
-    float v = calculate_v(dist, elapsed);
-    if (v <= vmax) {
-        cerr << "max speed is enough" << endl;
-        return vmax;
-    }
-    int m = 2;
-    while (v > vmax)
+    Zone()
     {
-        v = calculate_v(dist, elapsed * m);
-        cerr << "Speed : " << v << " M: " << m << endl;
-        m += 2;
+        duration = 0;
+        distance = 0;
+        time_speed = nullptr;
+        speeds = std::list<double>(500);
     }
-    return v;
+    int duration;
+    int distance;
+    double* time_speed;
+    std::list<double> speeds;
+};
+
+// check if the given duration will be within green light
+// interval 
+bool is_within_green(int t_cur, int t_max)
+{
+    int y = t_cur / t_max;
+    return y % 2 == 0;
 }
+/**
+ * Auto-generated code below aims at helping you parse
+ * the standard input according to the problem statement.
+ **/
+
 int main()
 {
     int speed;
@@ -62,43 +46,93 @@ int main()
     int lightCount;
     cin >> lightCount; cin.ignore();
     
-    float maxSpeed = speed / 3.6f;
-    
-    cerr << "Max speed (km/h): " << maxSpeed << endl;
-    float time_elapsed = 0;
-    float v_out = maxSpeed;
+    // number of lights, could have been named
+    // better
+    Zone* zones = new Zone[lightCount];
+    // index starts from 0 but we need the last value
+    // so for max speed 50, we need the index 50
+    const int speed_value_size = speed + 1;
+    // convert the speed limit to m/s
+    const double v_max_ms = (speed * 10.0) / 36.0;
     
     for (int i = 0; i < lightCount; i++) {
         int distance;
         int duration;
         cin >> distance >> duration; cin.ignore();
-        cerr << "Distance (meter) and duration(seconds): " 
-            << distance << " " << duration << endl;
         
-        float current_time = duration; //- time_elapsed;
-        cerr << "elapsed time from the light: " << current_time << endl;
+        zones[i].distance = distance;
+        zones[i].duration = duration;
+        // index at 50 is 49 so we need the extra size
+        zones[i].time_speed = new double[speed_value_size];
         
-        float v_cur = calc_speed(distance, maxSpeed, current_time, duration);
-        if (v_cur == maxSpeed)
+        // distance is starting from the start point
+        // so we need to substract from the previous point 
+        // to know the exact distance
+        if (i > 0)
+            distance -= zones[i-1].distance;
+            
+        for (int j = 1; j < speed_value_size; j++)
         {
-            cerr << " 2 " << endl;
-            v_cur = calc_speed(distance, v_out, current_time, duration);    
-        }
-        else if (i > 0 && v_out != maxSpeed)
-        {
-            cerr << " 1 " << endl;
-            float v_tmp = check_existing(v_out, distance, duration);
-            if (v_tmp != -1)
+            // X = V*t
+            double t_cur = ((distance * 36.0) / (10.0 * (double)j));
+            // set the current time it would take to traverse the distance
+            zones[i].time_speed[j] = t_cur;
+            if (i > 0)
             {
-                v_cur = v_tmp;
-                speed = v_cur;
+                // add the speed from the previous value
+                double h = zones[i - 1].time_speed[j];
+                t_cur += zones[i - 1].time_speed[j];
+                zones[i].time_speed[j] = t_cur;
+            }
+            if (double(distance) / t_cur > v_max_ms)
+            {
+                zones[i].speeds.push_back(0);
+                continue;
+            }
+            double r = t_cur / duration;
+            if (t_cur < duration)
+            {
+                zones[i].speeds.push_back(j);
+            }
+            else if (is_within_green(t_cur, duration))
+            {
+                zones[i].speeds.push_back(j);
+            }
+            else
+            {
+                zones[i].speeds.push_back(0);
             }
         }
-        
-        v_out = v_cur;
-        cerr << "tmp output " << v_out << endl;
-        time_elapsed += calculate_v(distance, v_cur);
     }
-    cerr << "output speed (km/h): " << v_out*3.6f << endl;
-    cout << (int)(v_out*3.6f) << endl;
+
+    if (lightCount == 1)
+    {
+        // there is only 1 light so no need to sort things
+        zones[0].speeds.sort();
+        cout << zones[0].speeds.back() << endl;
+    }
+    else
+    {
+        // hand given sorting that should be automated
+        std::list<double> v_test, v_temp;
+        for (int i = 0; i < lightCount - 1; i++)
+        {
+            zones[i].speeds.sort();
+            zones[i + 1].speeds.sort();
+            if (i == 0)
+            {
+                set_intersection(zones[i].speeds.begin(), zones[i].speeds.end(),
+                    zones[i + 1].speeds.begin(), zones[i + 1].speeds.end(), back_inserter(v_test));
+            }
+            else
+            {
+                set_intersection(zones[i + 1].speeds.begin(), zones[i + 1].speeds.end(),
+                    v_test.begin(), v_test.end(), back_inserter(v_temp));
+                v_test = v_temp;
+                v_temp.clear();
+            }
+            v_test.sort();
+        }
+        cout << (int)v_test.back() << endl;   
+    }
 }
